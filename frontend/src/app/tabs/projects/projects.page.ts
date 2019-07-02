91d4bf48572eed0cf55b85c5f51f5a17503b5687
Schanzenbaecher <jan.schanzenbaecher@sap.com>
2019-07-02 16:28:49 +0200

    Added edit and finish button

--- frontend/src/app/tabs/projects/projects.page.ts
+++ frontend/src/app/tabs/projects/projects.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Project } from '../../model/project';
 import { ProjectService } from '../../services/project.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-projects',
@@ -10,13 +11,46 @@ import { ProjectService } from '../../services/project.service';
 })
 export class ProjectsPage implements OnInit {
 
-  constructor(private router: Router, private projectService: ProjectService) { }
+  constructor(private router: Router, private projectService: ProjectService, public alertController: AlertController) { }
 
   public allProjects: Project[] = [];
   public newProject: Project = new Project();
 
   ngOnInit() {
     this.reloadAllProjects();
+  }
+
+  //on project finish button
+  async finish(project: Project){
+    const alert = await this.alertController.create({
+      header: 'Finish Project',
+      message: 'Are you sure you want to finish ' + project.title + '?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary'
+        }, {
+          text: 'Finish',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  //on project edit button
+  async edit(project: Project){
+    const alert = await this.alertController.create({
+      header: 'Edit Project',
+      message: 'This will lead to the project edit page of ' + project.title,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
   public reloadAllProjects() {
