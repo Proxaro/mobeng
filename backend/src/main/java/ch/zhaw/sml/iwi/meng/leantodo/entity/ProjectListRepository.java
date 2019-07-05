f9ebc0f13b1ed87da8f195f979132e20fe0a6a4c
Schanzenbaecher <jan.schanzenbaecher@sap.com>
2019-07-05 15:50:59 +0200

    rework SQL and ProjectList

--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/entity/ProjectListRepository.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/entity/ProjectListRepository.java
@@ -14,12 +14,11 @@ public interface ProjectListRepository extends JpaRepository<ProjectList, Long>
     /*@Query("select p.id, p.title, count(*) as total, sum(case when t.archived = true then 1 else 0 end) as finished from ToDo as t, Project as p where t.project = p.id and (p.owner = ?1 and p.archived = false) group by p.title")
     public List<Project> findAllButArchivedByOwner(String owner);*/
 
-    @Query(value="SELECT P.id, P.title, COUNT(TODO_T.id) as not_archived, COUNT(TODO_F.id) as archived " +
+    @Query(value="SELECT P.id, P.title, COUNT(TODO_D.id) as done, COUNT(TODO_T.id) as total " +
         "FROM Project P " +
-        "LEFT JOIN TO_DO TODO_F ON P.id = TODO_F.project " +
-            "AND TODO_F.archived = FALSE " +
         "LEFT JOIN TO_DO TODO_T ON P.id = TODO_T.project " +
-            "AND TODO_T.archived = TRUE " +
+        "LEFT JOIN TO_DO TODO_D ON P.id = TODO_D.project " +
+        "AND TODO_D.ARCHIVED = TRUE " +
         "WHERE P.owner = ?1 " +
         "GROUP BY P.id, P.title", nativeQuery = true)
     public List<ProjectList> findAllButArchivedByOwner(String owner);
