05dbae5617f60a0a974e798c4f02e07ea7573da5
Schanzenbaecher <jan.schanzenbaecher@sap.com>
2019-07-05 14:57:21 +0200

    minor fix to authentication module

--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/security/TokenAuthenticationFilter.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/security/TokenAuthenticationFilter.java
@@ -84,7 +84,7 @@ public class TokenAuthenticationFilter extends GenericFilterBean {
             SecurityContextHolder.getContext().setAuthentication(authentication);
             chain.doFilter(request, response);
         } catch (Exception e) {
-            LOGGER.info("Token could not be parsed! " + e.getMessage());
+            LOGGER.info("Token could not be parsed!");
             // We can stop here and abort the request. An attempt was made to log in with an
             // invalid token.
             ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token is invalid!");
